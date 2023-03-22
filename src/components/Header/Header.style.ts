3c91e23c3d8f463845ab63574f7ac75edba2d389
Daniel Peters <dan@brightec.co.uk>
2023-03-22 15:10:43 +0000

    Basic Set Up

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native/dist";
+import {Row, Col} from 'react-bootstrap'
+
+export const HeaderRow = styled(Row)`
+  background-color: aquamarine;
+`
+
+export const HeaderCol = styled(Col)`
+
+`
